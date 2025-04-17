529bbdc20ee79d0492801ea22c53065c26148822
Jerit Baiju <jeritalumkal@gmail.com>
2025-04-17 14:30:58 +0000

    update: response message in main function

--- functions/encrypt/src/main.py
+++ functions/encrypt/src/main.py
@@ -2,4 +2,4 @@ def main(context):
     if context.req.path == "/ping":
         return context.res.text("Pong")
 
-    return context.res.text("Hello World")
+    return context.res.text("Hello World from Python!")
